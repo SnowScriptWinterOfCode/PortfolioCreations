00b81dc2c62bccaa3d7264f269e0624454a30195
Ayushi Choudhary <73214455+ayushichoudhary-19@users.noreply.github.com>
2024-01-10 20:38:13 +0530

    Revert "Revert "Bugs Fixes and Discarded redundant code""

--- script.js
+++ script.js
@@ -1,63 +1,64 @@
 // ---------Responsive-navbar-active-animation-----------
-function test(){
-	var tabsNewAnim = $('#navbarSupportedContent');
-	var selectorNewAnim = $('#navbarSupportedContent').find('li').length;
-	var activeItemNewAnim = tabsNewAnim.find('.active');
-	var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
-	var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
-	var itemPosNewAnimTop = activeItemNewAnim.position();
-	var itemPosNewAnimLeft = activeItemNewAnim.position();
-	$(".hori-selector").css({
-		"top":itemPosNewAnimTop.top + "px", 
-		"left":itemPosNewAnimLeft.left + "px",
-		"height": activeWidthNewAnimHeight + "px",
-		"width": activeWidthNewAnimWidth + "px"
-	});
-	$("#navbarSupportedContent").on("click","li",function(e){
-		$('#navbarSupportedContent ul li').removeClass("active");
-		$(this).addClass('active');
-		var activeWidthNewAnimHeight = $(this).innerHeight();
-		var activeWidthNewAnimWidth = $(this).innerWidth();
-		var itemPosNewAnimTop = $(this).position();
-		var itemPosNewAnimLeft = $(this).position();
-		$(".hori-selector").css({
-			"top":itemPosNewAnimTop.top + "px", 
-			"left":itemPosNewAnimLeft.left + "px",
-			"height": activeWidthNewAnimHeight + "px",
-			"width": activeWidthNewAnimWidth + "px"
-		});
-	});
+function test() {
+  var tabsNewAnim = $("#navbarSupportedContent");
+  var selectorNewAnim = $("#navbarSupportedContent").find("li").length;
+  var activeItemNewAnim = tabsNewAnim.find(".active");
+  var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
+  var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+  var itemPosNewAnimTop = activeItemNewAnim.position();
+  var itemPosNewAnimLeft = activeItemNewAnim.position();
+  $(".hori-selector").css({
+    top: itemPosNewAnimTop.top + "px",
+    left: itemPosNewAnimLeft.left + "px",
+    height: activeWidthNewAnimHeight + "px",
+    width: activeWidthNewAnimWidth + "px",
+  });
+  $("#navbarSupportedContent").on("click", "li", function (e) {
+    $("#navbarSupportedContent ul li").removeClass("active");
+    $(this).addClass("active");
+    var activeWidthNewAnimHeight = $(this).innerHeight();
+    var activeWidthNewAnimWidth = $(this).innerWidth();
+    var itemPosNewAnimTop = $(this).position();
+    var itemPosNewAnimLeft = $(this).position();
+    $(".hori-selector").css({
+      top: itemPosNewAnimTop.top + "px",
+      left: itemPosNewAnimLeft.left + "px",
+      height: activeWidthNewAnimHeight + "px",
+      width: activeWidthNewAnimWidth + "px",
+    });
+  });
 }
-$(document).ready(function(){
-	setTimeout(function(){ test(); });
+$(document).ready(function () {
+  setTimeout(function () {
+    test();
+  });
 });
-$(window).on('resize', function(){
-	setTimeout(function(){ test(); }, 500);
+$(window).on("resize", function () {
+  setTimeout(function () {
+    test();
+  }, 500);
 });
-$(".navbar-toggler").click(function(){
-	$(".navbar-collapse").slideToggle(300);
-	setTimeout(function(){ test(); });
+$(".navbar-toggler").click(function () {
+  $(".navbar-collapse").slideToggle(300);
+  setTimeout(function () {
+    test();
+  });
 });
-
-
 
 // --------------add active class-on another-page move----------
-jQuery(document).ready(function($){
-	// Get current path and find target link
-	var path = window.location.pathname.split("/").pop();
+jQuery(document).ready(function ($) {
+  // Get current path and find target link
+  var path = window.location.pathname.split("/").pop();
 
-	// Account for home page with empty path
-	if ( path == '' ) {
-		path = 'index.html';
-	}
+  // Account for home page with empty path
+  if (path == "") {
+    path = "index.html";
+  }
 
-	var target = $('#navbarSupportedContent ul li a[href="'+path+'"]');
-	// Add active class to target link
-	target.parent().addClass('active');
+  var target = $('#navbarSupportedContent ul li a[href="' + path + '"]');
+  // Add active class to target link
+  target.parent().addClass("active");
 });
-
-
-
 
 // Add active class on another page linked
 // ==========================================
@@ -77,85 +78,133 @@ jQuery(document).ready(function($){
 //     })
 // });
 
-document.addEventListener("DOMContentLoaded", function() {
-    window.addEventListener("scroll", function() {
-        var scrollY = window.scrollY || document.documentElement.scrollTop;
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    var scrollY = window.scrollY || document.documentElement.scrollTop;
 
-        if (scrollY > 500) {
-            document.querySelector('.scroll-up-btn').classList.add("show");
-        } else {
-            document.querySelector('.scroll-up-btn').classList.remove("show");
-        }
-    });
+    if (scrollY > 500) {
+      document.querySelector(".scroll-up-btn").classList.add("show");
+    } else {
+      document.querySelector(".scroll-up-btn").classList.remove("show");
+    }
+  });
 
-    document.querySelector('.scroll-up-btn').addEventListener("click", function() {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
+  document
+    .querySelector(".scroll-up-btn")
+    .addEventListener("click", function () {
+      window.scrollTo({ top: 0, behavior: "smooth" });
     });
 });
 
+let scrollContainer = document.querySelector(".gallery");
+let backBtn = document.getElementById("backbutton");
+let nextBtn = document.getElementById("nextbutton");
 
-let scrollContainer = document.querySelector(".gallery"); 
-        let backBtn = document.getElementById("backbutton");
-        let nextBtn = document.getElementById("nextbutton");
+scrollContainer.addEventListener("wheel", (evt) => {
+  // evt.preventDefault();
+  scrollContainer.scrollleft += evt.deltaY;
+});
 
-        scrollContainer.addEventListener("wheel", (evt)=>{
-            // evt.preventDefault();
-            scrollContainer.scrollleft += evt.deltaY;
-        });
+nextBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft += 370;
+});
+backBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft -= 370;
+});
 
-        nextBtn.addEventListener("click", ()=>{
-
-            scrollContainer.style.scrollBehavior = "smooth";
-            scrollContainer.scrollLeft += 370;
-        });
-        backBtn.addEventListener("click", ()=>{
-
-            scrollContainer.style.scrollBehavior = "smooth";
-            scrollContainer.scrollLeft -= 370;
-        });
-
- profile
-		function displayImage(input) {
-            var file = input.files[0];
-            if (file) {
-                var reader = new FileReader();
-                reader.onload = function(e) {
-                    document.getElementById('uploadedImage').src = e.target.result;
-                };
-                reader.readAsDataURL(file);
-            }
-        }
+// profile;
+function displayImage(input) {
+  var file = input.files[0];
+  if (file) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      document.getElementById("uploadedImage").src = e.target.result;
+    };
+    reader.readAsDataURL(file);
+  }
+}
 
 //testimonial slider
-		// JavaScript for sliding through testimonials
-const slider = document.querySelector('.testimonial-slider');
+// JavaScript for sliding through testimonials
+const slider = document.querySelector(".testimonial-slider");
 
 let isDown = false;
 let startX;
 let scrollLeft;
 
-slider.addEventListener('mousedown', (e) => {
-    isDown = true;
-    slider.classList.add('active');
-    startX = e.pageX - slider.offsetLeft;
-    scrollLeft = slider.scrollLeft;
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.classList.add("active");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
 });
 
-slider.addEventListener('mouseleave', () => {
-    isDown = false;
-    slider.classList.remove('active');
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+  slider.classList.remove("active");
 });
 
-slider.addEventListener('mouseup', () => {
-    isDown = false;
-    slider.classList.remove('active');
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+  slider.classList.remove("active");
 });
 
-slider.addEventListener('mousemove', (e) => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - slider.offsetLeft;
-    const walk = (x - startX) * 2;
-    slider.scrollLeft = scrollLeft - walk;
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 2;
+  slider.scrollLeft = scrollLeft - walk;
 });
- main
+// main;
+
+// Syncing Active Navbar Links with Scroll
+
+let sec = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("#navbarSupportedContent ul li");
+let navMove = document.getElementsByClassName("hori-selector");
+
+window.onscroll = () => {
+  sec.forEach((part) => {
+    let top = window.scrollY;
+    let offset = part.offsetTop - 150;
+    let height = part.offsetHeight;
+    let id = part.getAttribute("id");
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("#navbarSupportedContent ul li[id*=li_" + id + "]")
+          .classList.add("active");
+        test();
+      });
+    }
+  });
+};
+
+// -------About me section number animation ---------
+
+function numIncrease(start, id, speed) {
+  elt = document.getElementById(id);
+  endNum = Number(document.getElementById(id).innerHTML);
+  incNumRec(start, endNum, elt, speed);
+}
+
+/*A recursive function to increase the number.*/
+function incNumRec(i, endNum, elt, speed) {
+  if (i <= endNum) {
+    elt.innerHTML = i;
+    setTimeout(function () {
+      //Delay a bit before calling the function again.
+      incNumRec(i + 1, endNum, elt, speed);
+    }, speed);
+  }
+}
+
+document.addEventListener("aos:in", () => {
+  numIncrease(0, "inc", 900);
+  numIncrease(24500, "speed", 5);
+  numIncrease(0, "tick", 200);
+});

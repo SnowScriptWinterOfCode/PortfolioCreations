cf9638d0e37d68303c0bddc8476ee2ef764b9d7c
Harshil Gupta <harshilgupta.2005@gmail.com>
2024-01-14 20:19:57 +0530

    Testimonial Changed

--- script.js
+++ script.js
@@ -234,5 +234,3 @@ window.onload = function () {
   css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
   document.body.appendChild(css);
 };
-
-// ----------- Testimonials -------------
